5ac4908762ae68241cc45ca85304d6e3a30882ef
Fernando <fernandondin@ciencias.unam.mx>
2021-04-02 03:12:41 -0600

    latexExplicacion

--- src/pi.py
+++ src/pi.py
@@ -12,13 +12,29 @@ def fact(n):
         f*=i
     return f
 s = "\\documentclass{article}\n"
+s+= "\\usepackage{amsmath}"
+s+= "\\usepackage{graphicx}"
 s+= "\\usepackage[utf8]{inputenc}\n"
 s+= "\\title{Aproximando $\\pi$}"
 s+= "\\author{Fernando Gerardo Flores García}\n"
 s+= "\\begin{document}\n"
 s+= "\\maketitle\n"
+s+= "\\section{Recordemos el teorema generalizado del binomio.}\n"
+s+= "\\begin{align*}"
+s+= "(y+x)^r = \\sum_{k=0}^{\\infty} \\binom{r}{k} y^{r-k}x^{k}\n"
+s+= "\\end{align*}\n\\begin{center}\ndonde r es cualquier real y\n"
+s+= "\\end{center}\n\\begin{align*}\n"
+s+= "\\binom{r}{k}=\\frac{1}{k!}\\prod_{n=0}^{k-1}(r-n)\n"
+s+= "\\end{align*}\n\\section{Ecuación de un circulo unitario}\n"
+s+= "\\begin{align*}\nx^2+y^2=1\\\\\n"
+s+= "\\end{align*}\n\\begin{center}\ndespejamos a y\n"
+s+= "\\end{center}\n\\begin{align*}\ny=(1-x^2)^\\frac{1}{2}\n"
+s+= "\\end{align*}\n"
+s+= "Notamos que podemos usar el teorema, "
 #***Menor complejidad al calcular el dividendo***
 def pi_mc(n):
+    global s
+    s+= "apliquémoslo a los primeros 11 términos.\n"
     x = symbols('x',commutative=False)
     dividendo = Fraction(1,2)
     exp = Fraction(1,1)
@@ -33,8 +49,7 @@ def pi_mc(n):
         exp_l = exp_l + ((dividendo * x**(i+2))/Rational(fact(i+2)))
         exp = exp + ((dividendo * y**(i+2))/Rational(fact(i+2)))
     pprint(exp)
-    global s
-    s+= "\\section{Obetenemos los "+str(n)+" terminos de la expresión}\n"
+    s+= "\\section{Obetenemos los "+str(n+1)+" terminos de la expresión}\n"
     s+= "$(1+x)^{\\frac{1}{2}}=$ "
     s+= "$" +str(latex(exp_l)) +"$\n"
     #s+= str(latex(pow((1+x),Fraction(1,2)))) + "\n"
@@ -44,12 +59,18 @@ def pi_mc(n):
     s+= "$(1-x^{2})^{\\frac{1}{2}}=$ "
     s+= "$"+str(latex(exp_l))+"$\n"
     s+= "\\section{Integrando de 0 a $\\frac{1}{2}$:}"
+    s+= "Estámos integrando para calcular el área de 0 a $\\frac{1}{2}$ del circulo.\\\\ \n"
     s+= "$ \\int_{0}^{\\frac{1}{2}} (1-x^{2})^{\\frac{1}{2}} \\,dx = "
     s+= "\\int_{0}^{\\frac{1}{2}} "+str(latex(exp_l))+"\\,dx $\n"
     exp = integrate(exp,(y,0,Fraction(1,2)))
     #exp_l = integrate(simplify(exp_l),(x,0,Fraction(1,2)))
     s+= "\\subparagraph{Resultado}\n"
+    s+= "\\includegraphics[width=0.5\\textwidth]{area_1.png}\n"
+    s+= "\\begin{flushleft}\n"
+    s+= "Sabemos que el área es $\\frac{\pi}{12} + \\frac{\\sqrt{3}}{8}$ entonces lo reemplazamos.\n"
+    s+= "\\end{flushleft}\n\\begin{flushleft}\n"
     s+= "$\\frac{\\pi}{2}+\\frac{\\sqrt{3}}{8}= "+latex(exp)+"$\n"
+    s+= "\\end{flushleft}\n"
     s+= "\\section{Despejando $\\pi$}\n"
     s+= "$\\pi="+"12["+str(latex(exp))+"-\\frac{\\sqrt{3}}{8}]$\n"
     print(exp_l)
@@ -59,13 +80,13 @@ def pi_mc(n):
     exp_l += -sqrt(3)/8
     exp_l *= 12
     s+= ", $\\pi="+str(latex(exp))+"$\n"
-    s+= "\\section{Aproximación final con "+str(n)+" iteraciones: }\n"
+    s+= "\\section{Aproximación final con "+str(n+1)+" Términos: }\n"
     s+= str(N(exp,40))+"\n"
     return simplify(exp)
 #hasta 46
 #print(N(pi_menor_complejidad(100),160))
 #print(s)
-print(N(pi_mc(10),40))
+print(N(pi_mc(16),40))
 s+= "\\end{document}"
 print(s)
 #pprint(integrate(exp,(x,0,Fraction(1,2))))
